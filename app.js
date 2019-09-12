19c5e47d03bc8c304f4482addfcbeabaa5d3cd6a
ksrajath7 <rajathkizhiyedam@Gmail.com>
2019-09-12 12:07:55 +0530

    Improved animations

--- app.js
+++ app.js
@@ -10,3 +10,6 @@ tl.fromTo(hero, 2, {width:"100%"}, {width:"80%", ease:Power2.easeInOut });
 
 tl.fromTo(slider, 2.3, {x:"-100%"}, {x:"0%", ease:Power2.easeInOut  }, "-=2");
 
+tl.fromTo(logo, 1, {opacity: 0, x:15}, {opacity: 1, x:0}, "-=1");
+tl.fromTo(hamburger, 1, {opacity: 0, x:-15}, {opacity: 1, x:0}, "-=1");
+tl.fromTo(headline, 1, {opacity: 0, x:50}, {opacity: 1, x:0}, "-=0.75");
